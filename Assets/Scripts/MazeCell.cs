591dc6609821f9a63f86c6088e4e197e04b22074
Ajinkya Kulkarni <quaintquid@gmail.com>
2015-06-22 19:31:07 +0530

    generates random maze but possibly buggy

---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeCell : MonoBehaviour {
+
+	public IntVector2 coordinates;
+	private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.Count];
+	
+	public MazeCellEdge GetEdge (MazeDirection direction) {
+		return edges[(int)direction];
+	}
+	
+	public void SetEdge (MazeDirection direction, MazeCellEdge edge) {
+		edges[(int)direction] = edge;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/MazeDirections.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class MazeDirections {
+
+	// Use this for initialization
+	public const int Count = 4;
+
+	public static MazeDirection RandomValue {
+		get {
+			return (MazeDirection)Random.Range(0, Count);
+		}
+	// Update is called once per frame
+	}
+
+	private static IntVector2[] vectors = {
+		new IntVector2 (0,1),
+		new IntVector2 (1,0),
+		new IntVector2 (0, -1),
+		new IntVector2 (-1, 0)
+	};
+
+	public static IntVector2 ToIntVector2 (this MazeDirection direction) {
+		return vectors[(int)direction];
+	}
+}
